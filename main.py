8b0dd082a3610066d43885b6192355e5bd60a7eb
Snowflake6413 <verycoolandsmart@protonmail.com>
2026-02-17 14:43:07 -0500

    Add: a simple about command

--- main.py
+++ main.py
@@ -34,8 +34,35 @@ def ping_pongs(say, event):
     lat = time.time() - float(msg_ts)
     say(f"Pong! Latency: {lat}")
 
-@app.command("/about")
+@app.command("/gatekeeper_about")
+def what_is_this_help_me(ack, respond, commannd):
+    ack()
 
+    about_blocks = [
+        		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Gatekeeper is a Slack bot that channel managers can use to help manage unwanted people from their channel."
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "You can manage your channel members and check if they meet your factors. If they dont, you can kick them out with one single command."
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "This is a WIP, more info to be added."
+			}
+		}
+    ]
+  
+    respond(blocks=about_blocks)
 
 
 
